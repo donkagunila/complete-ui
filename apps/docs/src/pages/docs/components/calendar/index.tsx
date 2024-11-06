e90a4340c8ba7c8724d7fad2a8fdc5fc953daf2c
Donald Kagunila <dkagunila@crdbbank.co.tz>
2024-11-06 10:02:35 +0300

    Setup component documentations

---
+++ apps/docs/src/pages/docs/components/calendar/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import DocsLayout from "@/components/layouts/DocsLayout";
+import PageSectionHeader from "@/components/app/page-section-header";
+
+const Main = () => {
+    return (
+        <DocsLayout>
+            <div>
+                <PageSectionHeader
+                    pageTitle="UI"
+                    title="Dialog"
+                    subTitle="A window overlaid on either the primary window or another dialog window, rendering the content underneath inert."/>
+            </div>
+
+        </DocsLayout>
+    );
+};
+
+export default Main;
---
+++ apps/docs/src/pages/docs/components/card/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import DocsLayout from "@/components/layouts/DocsLayout";
+import PageSectionHeader from "@/components/app/page-section-header";
+import {Callout} from "@complete-ui/core";
+
+const Main = () => {
+    return (
+        <DocsLayout>
+            <div>
+                <PageSectionHeader
+                    pageTitle="UI"
+                    title="Callout"
+                    subTitle="Short message to attract user’s attention."/>
+
+
+                <div className="border border-slate-200 py-12 px-5 rounded-md">
+                    <div className="flex flex-col gap-3 justify-center items-center">
+                        <Callout title="Test title" variant="primary">
+                            System Update: Enhanced Salesforce and Dynamics 365 integration now delivers
+                            key sales performance metrics directly to your dashboard for improved target
+                            achievement.
+                        </Callout>
+
+                        <Callout title="Test title" variant="success">
+                            System Update: Enhanced Salesforce and Dynamics 365 integration now delivers
+                            key sales performance metrics directly to your dashboard for improved target
+                            achievement.
+                        </Callout>
+
+                        <Callout title="Test title" variant="warning">
+                            System Update: Enhanced Salesforce and Dynamics 365 integration now delivers
+                            key sales performance metrics directly to your dashboard for improved target
+                            achievement.
+                        </Callout>
+
+                        <Callout title="Test title" variant="neutral">
+                            System Update: Enhanced Salesforce and Dynamics 365 integration now delivers
+                            key sales performance metrics directly to your dashboard for improved target
+                            achievement.
+                        </Callout>
+
+                        <Callout title="Test title" variant="error">
+                            System Update: Enhanced Salesforce and Dynamics 365 integration now delivers
+                            key sales performance metrics directly to your dashboard for improved target
+                            achievement.
+                        </Callout>
+                    </div>
+                </div>
+            </div>
+
+        </DocsLayout>
+    );
+};
+
+export default Main;
