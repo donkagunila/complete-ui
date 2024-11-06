8580c341ebd4646d044d4b6026b523e8a34401d5
Donald Kagunila <dkagunila@crdbbank.co.tz>
2024-11-06 10:06:49 +0300

    Change Next cnonfigurations

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.css";
 import type {AppProps} from "next/app";
-import "@complete-ui/core/styles.css"
+import "@complete-ui/core/styles.css";
+import "@/styles/globals.css";
 
 export default function App({Component, pageProps}: AppProps) {
     return <Component {...pageProps} />;
